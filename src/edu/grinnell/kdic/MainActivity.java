da1b053bcb3ac778046b978646b4c4a6df0570d2
Spencer B Liberto <libertos@grinnell.edu>
2012-10-24 17:48:23 -0500

    10/24
    Added BannerView and a whole bunch of junk.

--- src/edu/grinnell/kdic/MainActivity.java
+++ src/edu/grinnell/kdic/MainActivity.java
@@ -1,85 +1,105 @@
 package edu.grinnell.kdic;
 
-import java.net.URI;
-import java.util.List;
-import net.moraleboost.streamscraper.Stream;
-import net.moraleboost.streamscraper.Scraper;
-import net.moraleboost.streamscraper.scraper.IceCastScraper;
 
 
 import java.io.IOException;
-
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 
 public class MainActivity extends Activity {
 
-	private static final String URL = "http://kdic.grinnell.edu:8001/kdic128";
+	private static final String STREAMURL = "http://kdic.grinnell.edu:8001/kdic128"; //KDIC stream URL
+	private static final String METAURL = "http://kdic.grinnell.edu:8001/"; //metadata URL
+	private static final String PICURL = "http://kdic.grinnell.edu/wp-content/uploads/radio-300x199.jpg"; //an arbitrary picture, for testing metadata
     
-    MediaPlayer mediaPlayer = new MediaPlayer();
+    private MediaPlayer kdicStream = new MediaPlayer();
+    private ImageView curPlayingImage;
+    private Button playButton;
     
+    boolean streamStopped = true;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        final Button playPause = (Button) findViewById(R.id.playButton);
-        playPause.setText("Pause");
-        playPause.setOnClickListener(new View.OnClickListener() {
+        //playPause button junk, setting text and creating listener
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setText("Pause");
+        playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 playPause();
             }
         });
         
+        //stream listener, solely for changing the button text from 'loading' to 'pause' after it finishes loading.
+        kdicStream.setOnPreparedListener(new android.media.MediaPlayer.OnPreparedListener() {
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+	            playButton.setText("Pause");
+			}
+		});
+        
+        //Starts Stream
         setupPlayer();
-        startPlaying();
+        if (streamStopped == true){
+        	playPause();
+        }
+        
         
     }
-
     
     public void playPause(){
-    	final Button playPause = (Button) findViewById(R.id.playButton);
-    	if(mediaPlayer.isPlaying()){
+    	
+    	//If the stream is loading or playing, stop it. Else, start it.
+    	if((streamStopped == false) && kdicStream.isPlaying() ){
         	stopPlaying();
-            playPause.setText("Play");
-    	}else{
+        	streamStopped = true;
+            playButton.setText("Play");
+    	}else if(streamStopped == true){
+    		streamStopped = false;
+    		playButton.setText("Starting..."); // WHY WON'T THIS APPEAR FOR MORE THAN A FRACTION OF A SECOND.
         	startPlaying();
-            playPause.setText("Pause");
     	}
     }
     
     public boolean startPlaying(){
     	
+    	//prepare and start stream
     	try {
-			mediaPlayer.prepare();
+    		kdicStream.prepare();
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} // might take long! (for buffering, etc)
         
-        mediaPlayer.start();
+       kdicStream.start();
         
         return true;
     }
     
     public boolean stopPlaying(){
-    	mediaPlayer.stop();
+    	//STOP
+    	kdicStream.stop();
     	return true;
     }
     
  
     public void setupPlayer(){
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    	//Setup stream type and URL
+        kdicStream.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
-			mediaPlayer.setDataSource(URL);
+			kdicStream.setDataSource(STREAMURL);
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (SecurityException e) {
@@ -90,27 +110,6 @@ public class MainActivity extends Activity {
 			e.printStackTrace();
 		}
         
-        /*
-        if(Integer.valueOf(android.os.Build.VERSION.SDK) >= 10)
-        	setupMetadata();
-        	*/
     }
-    
-    /*
-    @TargetApi(10)
-	public void setupMetadata(){
-    	final MediaMetadataRetriever metaGet = new MediaMetadataRetriever();
-    	final TextView t = (TextView) findViewById(R.id.status);
-    	mediaPlayer.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-            @Override
-            public boolean onInfo(MediaPlayer mp, int what, int extra) {                     
-                if (what == MediaPlayer.MEDIA_INFO_METADATA_UPDATE) {
-                    t.setText(metaGet.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
-                }
-				return true;          
-            }
-        });
-    }
-    */
     
 }
